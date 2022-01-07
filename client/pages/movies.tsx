7d34e7eec97360f13f620a95a58071a1081b6af8
metesv <metehansivri1@gmail.com>
2022-01-08 01:55:40 +0300

    Initial setup

---
+++ client/pages/movies.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Movies: NextPage<any> = () => {
+  return (
+    <>
+      <h1>Hello from Movies</h1>
+    </>
+  )
+}
+
+export default Movies;
---
+++ client/pages/series.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Series: NextPage<any> = () => {
+  return (
+    <>
+      <h1>Hello from Series</h1>
+    </>
+  )
+}
+
+export default Series;
